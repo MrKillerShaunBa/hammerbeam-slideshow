129fdf6a0f1a82b83ccbfe284e2a9f7e3cddd868
MrKillerShaunBa <122672193+MrKillerShaunBa@users.noreply.github.com>
2025-12-24 00:34:37 +0530

    Change canvas buffer types to uint8_t
    Updated canvas buffers to be uint8_t for LVGL v8 compliance.

--- boards/shields/nice_view_custom/widgets/status.h
+++ boards/shields/nice_view_custom/widgets/status.h
@@ -14,9 +14,12 @@
 struct zmk_widget_status {
     sys_snode_t node;
     lv_obj_t *obj;
-    lv_color_t cbuf[CANVAS_SIZE * CANVAS_SIZE];
-    lv_color_t cbuf2[CANVAS_SIZE * CANVAS_SIZE];
-    lv_color_t cbuf3[CANVAS_SIZE * CANVAS_SIZE];
+
+    /* Indexed canvas buffers (LVGL v8 compliant) */
+    uint8_t cbuf[CANVAS_SIZE * CANVAS_SIZE];
+    uint8_t cbuf2[CANVAS_SIZE * CANVAS_SIZE];
+    uint8_t cbuf3[CANVAS_SIZE * CANVAS_SIZE];
+
     struct status_state state;
 };
 
