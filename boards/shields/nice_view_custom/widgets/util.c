f35b5bf1f6b40e7724ab9a55aeb73f3007472428
MrKillerShaunBa <122672193+MrKillerShaunBa@users.noreply.github.com>
2025-12-24 00:32:12 +0530

    Refactor canvas rotation for indexed image support
    Updated canvas rotation to support 1-bit indexed images with a palette. Added palette for indexed images and adjusted image structure accordingly.

--- boards/shields/nice_view_custom/widgets/util.c
+++ boards/shields/nice_view_custom/widgets/util.c
@@ -6,27 +6,56 @@
  */
 
 #include <zephyr/kernel.h>
+#include <string.h>
 #include "util.h"
 
 LV_IMG_DECLARE(bolt);
 
+/* Palette for 1-bit indexed images (required in LVGL v8) */
+static const lv_color_t mono_palette[] = {
+    LV_COLOR_BLACK,
+    LV_COLOR_WHITE,
+};
+
 void rotate_canvas(lv_obj_t *canvas, lv_color_t cbuf[]) {
     static lv_color_t cbuf_tmp[CANVAS_SIZE * CANVAS_SIZE];
+
     memcpy(cbuf_tmp, cbuf, sizeof(cbuf_tmp));
-    lv_img_dsc_t img;
-    img.data = (void *)cbuf_tmp;
-    img.header.cf = LV_IMG_CF_TRUE_COLOR;
-    img.header.w = CANVAS_SIZE;
-    img.header.h = CANVAS_SIZE;
+
+    lv_img_dsc_t img = {
+        .header = {
+            .cf = LV_IMG_CF_INDEXED_1_BIT,
+            .w = CANVAS_SIZE,
+            .h = CANVAS_SIZE,
+        },
+        .data_size = sizeof(cbuf_tmp),
+        .data = (const uint8_t *)cbuf_tmp,
+    };
+
+    /* Attach palette (LVGL v8 requirement) */
+    img.header.always_zero = 0;
+    img.data = (const uint8_t *)cbuf_tmp;
+    img.palette = mono_palette;
 
     lv_canvas_fill_bg(canvas, LVGL_BACKGROUND, LV_OPA_COVER);
-    lv_canvas_transform(canvas, &img, 900, LV_IMG_ZOOM_NONE, -1, 0, CANVAS_SIZE / 2,
-                        CANVAS_SIZE / 2, true);
+
+    lv_canvas_transform(
+        canvas,
+        &img,
+        900,                /* 90 degrees */
+        LV_IMG_ZOOM_NONE,
+        -1,
+        0,
+        CANVAS_SIZE / 2,
+        CANVAS_SIZE / 2,
+        true
+    );
 }
 
 void draw_battery(lv_obj_t *canvas, const struct status_state *state) {
     lv_draw_rect_dsc_t rect_black_dsc;
     init_rect_dsc(&rect_black_dsc, LVGL_BACKGROUND);
+
     lv_draw_rect_dsc_t rect_white_dsc;
     init_rect_dsc(&rect_white_dsc, LVGL_FOREGROUND);
 
@@ -43,8 +72,8 @@ void draw_battery(lv_obj_t *canvas, const struct status_state *state) {
     }
 }
 
-void init_label_dsc(lv_draw_label_dsc_t *label_dsc, lv_color_t color, const lv_font_t *font,
-                    lv_text_align_t align) {
+void init_label_dsc(lv_draw_label_dsc_t *label_dsc, lv_color_t color,
+                    const lv_font_t *font, lv_text_align_t align) {
     lv_draw_label_dsc_init(label_dsc);
     label_dsc->color = color;
     label_dsc->font = font;
